bdd7b63be3c64987c8031193e88016b682608bff
Panwar Vivek <vpanwa@codeaurora.org>
2016-01-25 20:31:59 +0530

    Enabled flag VERITY
    Enabled flag VERITY
    
    Change-Id: Ib0d56b9e0ef88050c115091639e9afac2ba02f25

--- titanium_64.mk
+++ titanium_64.mk
@@ -185,7 +185,7 @@ endif
      device/qcom/titanium_64/sensors/hals.conf:system/etc/sensors/hals.conf
 
 # Disable Verity boot feature
-PRODUCT_SUPPORTS_VERITY := false
+PRODUCT_SUPPORTS_VERITY := true
 
 # Reduce client buffer size for fast audio output tracks
 PRODUCT_PROPERTY_OVERRIDES += \
