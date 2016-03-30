7b1a549538d289b8325e1f255a364e75c7ddd9d1
Ashish Jain <ashishj@codeaurora.org>
2016-03-30 13:35:38 +0530

    msm8953_64: Move audio configuration setup to HAL folder.
    -Refer to makefile in audio HAL folder to setup audio
    configuration.
    -Move audio config files to HAL folder.
    -Move all audio specific properties from system.prop
    to audio hal configs folder.
    
    Change-Id: I1d15b4410812a3b07b553059d55d7f11ad7ddb6e

--- titanium_64.mk
+++ titanium_64.mk
@@ -60,37 +60,8 @@ endif
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
-PRODUCT_PACKAGES += \
-    libqcomvisualizer \
-    libqcompostprocbundle \
-    libqcomvoiceprocessing
-
 # Audio configuration file
-PRODUCT_COPY_FILES += \
-    device/qcom/titanium_32/audio_policy.conf:system/etc/audio_policy.conf \
-    device/qcom/titanium_32/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-    device/qcom/titanium_32/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    device/qcom/titanium_32/mixer_paths_mtp.xml:system/etc/mixer_paths_mtp.xml \
-    device/qcom/titanium_32/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
-    device/qcom/titanium_32/mixer_paths_qrd_skui.xml:system/etc/mixer_paths_qrd_skui.xml \
-    device/qcom/titanium_32/mixer_paths_qrd_skuhf.xml:system/etc/mixer_paths_qrd_skuhf.xml \
-    device/qcom/titanium_32/mixer_paths_qrd_skum.xml:system/etc/mixer_paths_qrd_skum.xml \
-    device/qcom/titanium_32/mixer_paths_qrd_sku3.xml:system/etc/mixer_paths_qrd_sku3.xml \
-    device/qcom/titanium_32/mixer_paths_wcd9330.xml:system/etc/mixer_paths_wcd9330.xml \
-    device/qcom/titanium_32/mixer_paths_wcd9306.xml:system/etc/mixer_paths_wcd9306.xml \
-    device/qcom/titanium_32/mixer_paths_wcd9335.xml:system/etc/mixer_paths_wcd9335.xml \
-    device/qcom/titanium_32/mixer_paths_wcd9326.xml:system/etc/mixer_paths_wcd9326.xml \
-    device/qcom/titanium_32/mixer_paths_qrd_skun.xml:system/etc/mixer_paths_qrd_skun.xml \
-    device/qcom/titanium_32/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
-    device/qcom/titanium_32/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/qcom/titanium_32/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    device/qcom/titanium_32/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-    device/qcom/titanium_32/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
-    device/qcom/titanium_32/sound_trigger_mixer_paths_wcd9335.xml:system/etc/sound_trigger_mixer_paths_wcd9335.xml \
-    device/qcom/titanium_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
-    device/qcom/titanium_32/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    device/qcom/titanium_32/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
-    device/qcom/titanium_64/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+-include $(TOPDIR)hardware/qcom/audio/configs/msm8953/msm8953.mk
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
@@ -184,10 +155,3 @@ endif
 # Disable Verity boot feature
 PRODUCT_SUPPORTS_VERITY := true
 
-# Reduce client buffer size for fast audio output tracks
-PRODUCT_PROPERTY_OVERRIDES += \
-     af.fast_track_multiplier=1
-
-# Low latency audio buffer size in frames
-PRODUCT_PROPERTY_OVERRIDES += \
-    audio_hal.period_size=192
