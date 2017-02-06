35ea9c5c2985081e2439139584c33662ca213117
Bharat Pawar <bpawar@codeaurora.org>
2017-02-06 15:17:29 +0530

    msm8953_64: Switch to pure AOSP framework configuration for initial bring-up
    Set flag to indicate pure AOSP configuration for new source
    bring-up.  Also disable various feature until modules are
    brought-up.
    
    Change-Id: Iab66ef412f4e59e8442eda67da4cf21dcfcba30d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,6 @@ PROTOBUF_SUPPORTED := false
 
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
-TARGET_USES_QCOM_BSP := true
 TARGET_NO_RPC := true
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000
@@ -117,8 +116,15 @@ USE_SENSOR_MULTI_HAL := true
 #Enable peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
+ifeq ($(TARGET_USES_AOSP), true)
+TARGET_HW_DISK_ENCRYPTION := false
+else
+# SDClang configuration
+SDCLANG := true
 #Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION := true
+endif
+
 TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 
 #Enabling IMS Feature
--- msm8953_64.mk
+++ msm8953_64.mk
@@ -1,16 +1,22 @@
-DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8953_64/overlay
+TARGET_USES_AOSP := true
+TARGET_USES_QCOM_BSP := false
 
-TARGET_USES_QCOM_BSP := true
-#BOARD_HAVE_QCOM_FM := true
-# Add QC Video Enhancements flag
+ifeq ($(TARGET_USES_AOSP),true)
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := false
+TARGET_USES_QTIC := false
+else
+DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8953_64/overlay
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-TARGET_USES_NQ_NFC := true
-TARGET_KERNEL_VERSION := 3.18
-#QTIC flag
+TARGET_USES_QTIC := true
 -include $(QCPATH)/common/config/qtic-config.mk
+endif
+
+#BOARD_HAVE_QCOM_FM := true
+TARGET_USES_NQ_NFC := false
+TARGET_KERNEL_VERSION := 3.18
 
 # Enable features in video HAL that can compile only on this platform
-TARGET_USES_MEDIA_EXTENSIONS := true
+TARGET_USES_MEDIA_EXTENSIONS := false
 
 # media_profiles and media_codecs xmls for msm8953
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
@@ -33,10 +39,10 @@ PRODUCT_DEVICE := msm8953_64
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := msm8953 for arm64
 
-PRODUCT_BOOT_JARS += tcmiface
+#PRODUCT_BOOT_JARS += tcmiface
 
 ifneq ($(strip $(QCPATH)),)
-PRODUCT_BOOT_JARS += WfdCommon
+#PRODUCT_BOOT_JARS += WfdCommon
 #PRODUCT_BOOT_JARS += com.qti.dpmframework
 #PRODUCT_BOOT_JARS += dpmapi
 #PRODUCT_BOOT_JARS += com.qti.location.sdk
@@ -106,7 +112,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
+#PRODUCT_BOOT_JARS += telephony-ext
 
 # Defined the locales
 PRODUCT_LOCALES += th_TH vi_VN tl_PH hi_IN ar_EG ru_RU tr_TR pt_BR bn_IN mr_IN ta_IN te_IN zh_HK \
@@ -117,7 +123,7 @@ PRODUCT_LOCALES += th_TH vi_VN tl_PH hi_IN ar_EG ru_RU tr_TR pt_BR bn_IN mr_IN t
 #PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
 #        $(QCPATH)/qrdplus/globalization/multi-language/res-overlay \
 #        $(PRODUCT_PACKAGE_OVERLAYS)
-PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
+#PRODUCT_PACKAGE_OVERLAYS := $(QCPATH)/qrdplus/Extension/res \
         $(PRODUCT_PACKAGE_OVERLAYS)
 
 #for android_filesystem_config.h
