0ce09fd311e8a72499c6d7c143801c10ae623f36
Anurag Chouhan <achouhan@codeaurora.org>
2017-06-23 16:36:38 +0530

    msm8953_64: Change system/etc to /vendor/etc
    . Change symbolic link from /system/etc to /vendor for ini and nv files
    . Change wcnss-service binary location from /system/bin to /vendor/bin
    . Change copy location form /system/etc to /vendor/etc.
    
    Change-Id: I10d990150a1b229d5e5db3825c7631976322603c
    CRs-fixed: 2082793

--- msm8953_64.mk
+++ msm8953_64.mk
@@ -140,7 +140,7 @@ PRODUCT_COPY_FILES += \
 
 #wlan driver
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8953_64/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    device/qcom/msm8953_64/WCNSS_qcom_cfg.ini:vendor/etc/wifi/WCNSS_qcom_cfg.ini \
     device/qcom/msm8953_32/WCNSS_wlan_dictionary.dat:persist/WCNSS_wlan_dictionary.dat \
     device/qcom/msm8953_64/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
 
