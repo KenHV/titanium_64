20e626663ad1384e4518ebd4d779989d54e94adc
Mahantesh Sanakall <msanak@codeaurora.org>
2017-08-08 11:19:08 +0530

    Rename the device manifest file.
    Rename the device manifest(vintf.xml) to manifest.xml and move
    away from building device manifest through PRODUCT_COPY_FILES.
    (It will be built through another change using the build
    variable - DEVICE_MANIFEST_FILE).
    
    Change-Id: Ie577cbb2199a17d0b8836f6907dc1d850dce0aa5

--- msm8953_64.mk
+++ msm8953_64.mk
@@ -82,7 +82,7 @@ endif #BOARD_HAVE_QCOM_FM
 
 # add vendor manifest file
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8953_64/vintf.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
+    device/qcom/msm8953_64/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
 
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
