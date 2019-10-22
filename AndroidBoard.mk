7a7a2d7a83582e5218f7463bccb2e568d8f61338
Naitik Bharadiya <bharad@codeaurora.org>
2019-10-22 18:11:31 +0530

    msm8953_64: Enable LLVM and point to the correct path for kernel defconfig
    Enable LLVM compilation for msm8953_64 and point the Android makefile
    to the new path for the kernel-4.9 defconfig.
    
    Change-Id: I8d825a62258d3be1787e2325730771d9cd5f3eaa

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -28,9 +28,9 @@ ifeq ($(KERNEL_DEFCONFIG),)
         endif
     else ifneq ($(wildcard kernel/msm-4.9),)
         ifeq ($(TARGET_BUILD_VARIANT),user)
-          KERNEL_DEFCONFIG := msm8953-perf_defconfig
+          KERNEL_DEFCONFIG := vendor/msm8953-perf_defconfig
         else
-          KERNEL_DEFCONFIG := msm8953_defconfig
+          KERNEL_DEFCONFIG := vendor/msm8953_defconfig
         endif
     endif
 endif
